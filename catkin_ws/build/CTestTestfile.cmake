ad091ddd28450d7e896d453e7106ea0ca523b352
Iuri07 <iuricostermani@hotmail.com>
2017-07-17 17:43:51 -0300

    catkin_ws added to repo

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/iuro/catkin_ws/src
+# Build directory: /home/iuro/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(rbx1/rbx1)
+SUBDIRS(rbx1/rbx1_bringup)
+SUBDIRS(rbx1/rbx1_dynamixels)
+SUBDIRS(rbx1/rbx1_speech)
+SUBDIRS(rbx1/rbx1_vision)
+SUBDIRS(rbx1/rbx1_apps)
+SUBDIRS(rbx1/rbx1_nav)
+SUBDIRS(rbx1/rbx1_description)
+SUBDIRS(usb_cam)
